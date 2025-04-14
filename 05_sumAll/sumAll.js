cf4c9fe390772aab26f4176748981964f1e78935
Kunal-Vala <kunalvala2005@gmail.com>
2025-04-14 13:41:35 +0530

    DONE 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+    if (!Number.isInteger(n1) || !Number.isInteger(n2)) return "ERROR";
+    if (n1 < 0 || n2 < 0) return "ERROR";
+    if (n1 > n2) {
+        [n1, n2] = [n2, n1];
+    }
+    let sum = 0;
+    for (let i = n1; i <= n2; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
