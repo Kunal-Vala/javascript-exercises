46ceab9d793b0940c44a59d7cf814072b42e27ad
Kunal-Vala <kunalvala2005@gmail.com>
2025-04-14 13:24:02 +0530

    DONE 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+// const removeFromArray = function(arr,...values) {
+//     let newArray = [];
+
+//     arr.forEach((item) => {
+//         if(!values.includes(item)){
+//             newArray.push(item);
+//         }
+//     });
+//     return newArray;
+// };
+
+const removeFromArray = function(array, ...args) {
+  return array.filter(val => !args.includes(val))
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
