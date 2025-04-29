3a52176d192119aa0e01066fff3336d685e1f28a
Kunal-Vala <kunalvala2005@gmail.com>
2025-04-29 16:20:51 +0530

    08-calculator -> DONE

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const sumArr = arr.reduce((total,item) => total + item,0);
+  return sumArr;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	const mulArr = arr.reduce((total,item) => total * item,1);
+  return mulArr;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let ans = 1;
+	for(let i = 1;i<= b;i++){
+    ans = a*ans;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let flag = 1;
+  for(let i =1;i<= a;i++){
+    flag = flag*i;
+  }
+  return flag;
 };
 
 // Do not edit below this line
